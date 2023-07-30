5ced287c53cbeaeaab9d53f743a789c345718d4d
Emil Belyov <emilbelyov@MacBook-Pro.local>
2023-07-30 12:38:24 +0300

    feat: added project 6 video

--- build/preproc/ctags_target_for_gcc_minus_e.cpp
+++ build/preproc/ctags_target_for_gcc_minus_e.cpp
@@ -4,39 +4,13 @@
 Empty project skeleton
 
 *************************************************************/
-# 4 "/Users/emilbelyov/Projects/Arduino Project/Arduino/Arduino.ino"
-int sensorValue;
-int sensorLow = 1023;
-int sensorHigh = 0;
-
-const int ledPin = 13;
-
+# 5 "/Users/emilbelyov/Projects/Arduino Project/Arduino/Arduino.ino"
 void setup()
 {
-    pinMode(ledPin, 0x1);
-    digitalWrite(ledPin, 0x1);
 
-    while (millis() < 5000)
-    {
-        sensorValue = analogRead(A0);
-        if (sensorValue > sensorHigh) {
-            sensorHigh = sensorValue;
-        }
-
-        if (sensorValue < sensorLow) {
-            sensorLow = sensorValue;
-        }
-    }
-
-    digitalWrite(ledPin, 0x0);
 }
 
 void loop()
 {
-    sensorValue = analogRead(A0);
-    int pitch = map(sensorValue, sensorLow, sensorHigh, 50, 4000);
 
-    tone(8, pitch, 20);
-
-    delay(10);
 }
--- build/sketch/Arduino.ino.cpp
+++ build/sketch/Arduino.ino.cpp
@@ -3,43 +3,18 @@
 /***********************************************************
 Empty project skeleton
 *************************************************************/
-int sensorValue;
-int sensorLow = 1023;
-int sensorHigh = 0;
 
-const int ledPin = 13;
-
-#line 10 "/Users/emilbelyov/Projects/Arduino Project/Arduino/Arduino.ino"
+#line 5 "/Users/emilbelyov/Projects/Arduino Project/Arduino/Arduino.ino"
 void setup();
-#line 30 "/Users/emilbelyov/Projects/Arduino Project/Arduino/Arduino.ino"
-void loop();
 #line 10 "/Users/emilbelyov/Projects/Arduino Project/Arduino/Arduino.ino"
+void loop();
+#line 5 "/Users/emilbelyov/Projects/Arduino Project/Arduino/Arduino.ino"
 void setup()
 {
-    pinMode(ledPin, OUTPUT);
-    digitalWrite(ledPin, HIGH);
 
-    while (millis() < 5000)
-    {
-        sensorValue = analogRead(A0);
-        if (sensorValue > sensorHigh) {
-            sensorHigh = sensorValue;
-        }
-
-        if (sensorValue < sensorLow) {
-            sensorLow = sensorValue;
-        }
-    }
-
-    digitalWrite(ledPin, LOW);
 }
 
 void loop()
 {
-    sensorValue = analogRead(A0);
-    int pitch = map(sensorValue, sensorLow, sensorHigh, 50, 4000);
 
-    tone(8, pitch, 20);
-
-    delay(10);
 }
